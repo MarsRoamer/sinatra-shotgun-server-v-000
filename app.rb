d37284636f71d5c17ac3481599405cf0035051fb
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-23 19:11:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!!, Did this make the cut? "
+    "Welcome to your app!!!!, Did this make the cut? This is really cool, shotgun is awesome! "
   end
 
 end
